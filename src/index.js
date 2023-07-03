d8ce17ecc644bc21668907509fa44084d55031be
Abdullah080 <abdullah.mohammed08@outlook.com>
2023-07-03 14:25:55 +0400

    contact section + footer are done

--- src/index.js
+++ src/index.js
@@ -3,3 +3,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/style.css";
 import "@fortawesome/fontawesome-free/js/all.min.js";
 import "./sass/style.scss";
+
+// Current Year
+document.getElementById("copyright").innerHTML = "Abdullah All Right Reversed" + " " + new Date().getFullYear();
