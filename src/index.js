2b1e3a71877060601efc83ea12d26d166817fdec
Abdullah080 <abdullah.mohammed08@outlook.com>
2023-07-04 10:49:15 +0400

    complete resposive design + add scroll up feature

--- src/index.js
+++ src/index.js
@@ -6,3 +6,22 @@ import "./sass/style.scss";
 
 // Current Year
 document.getElementById("copyright").innerHTML = "Abdullah All Right Reversed" + " " + new Date().getFullYear();
+
+// Scroll to Up
+let scrollUp = document.getElementById("up");
+
+window.onscroll = function () {
+  console.log(this.scrollY);
+  if (this.scrollY >= 700) {
+    scrollUp.classList.add("show");
+  } else {
+    scrollUp.classList.remove("show");
+  }
+};
+
+scrollUp.onclick = function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
